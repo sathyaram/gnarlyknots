9a81504db9a980bc0333d6c165d99d48f9cca344
Sathya Ram <sathyaram@Sathyas-MacBook-Pro.local>
2021-03-06 14:56:56 -0500

    More texture additions and responsivizing

--- src/themes/gnarlyknots/index.php
+++ src/themes/gnarlyknots/index.php
@@ -25,7 +25,7 @@
 			<div class="container-small">
 				<div class="about-text">
 					<h3>We gave the <span>Pretzel</span> some upgrades</h3>
-					<p>Gnarly Knots Pretzel Company is a little pretzel palace in the Western suburbs of Illinous. We turned the idea of what a Pretzel can be upside down. We put all your favorite dinners, lunches, breakfasts and even desserts INSIDE the pretzel! In the mood for Italian Beef… we got it. Looking for a Salami & Provolone… No problem! How about Bacon, Tater tots and Cheese… heck yeah! Chocolate chip cookie dough or Oreo Cheesecake? You bet your sweet bippy! No worries, we also bake the best authentically German-style salted pretzels you will ever eat!
+					<p>Gnarly Knots Pretzel Company is a little pretzel palace in the Western suburbs of Illinous. We turned the idea of what a Pretzel can be upside down. Our award-winning pretzels put all your favorite dinners, lunches, breakfasts and even desserts INSIDE the pretzel! In the mood for Italian Beef… we got it. Looking for a Salami & Provolone… No problem! How about Bacon, Tater tots and Cheese… heck yeah! Chocolate chip cookie dough or Oreo Cheesecake? You bet your sweet bippy! No worries, we also bake the best authentically German-style salted pretzels - stop in today for the best pretzels you will ever eat!
 					</p>
 				</div>
 				<div class="about-image">
@@ -33,7 +33,9 @@
 			</div>
 		</section>
 		<section class="eat-more-pretzels">
-			<div class="section-headline">Eat More Pretzels</div>
+			<div class="section-headline"><span>Eat More Pretzels</span>
+				<a class="gnarly-button" href="">Buy a Gift Card Today!</a>
+			</div>
 			<div class="photo-grid">
 				<div class="grid-photo">
 				</div>
@@ -88,6 +90,115 @@
 							<div class="menu-name">Three Cheese Stuffed</div>
 							<div class="menu-desc">It's delicious.</div>
 							<div class="menu-price">7</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="daily-soups">
+			<div class="container">
+				<div class="section-header">
+					<h3>Daily Soups</h3>
+					<p>Our delicious mouth-watering soups that go hand in hand with your favorite pretzel.</p>
+				</div>
+				<div class="section-content">
+					<div class="cards">
+						<div class="card">
+							<div class="card-header">
+								<div class="menu-day">
+									Monday
+								</div>
+								<div class="menu-price">
+									8
+								</div>
+							</div>
+							<div class="menu-name">
+								French Onion
+							</div>
+							<div class="menu-desc">
+								Creamy blends of goodness.
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header">
+								<div class="menu-day">
+									Tuesday
+								</div>
+								<div class="menu-price">
+									9
+								</div>
+							</div>
+							<div class="menu-name">
+								Black Bean
+							</div>
+							<div class="menu-desc">
+								Creamy blends of goodness.
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header">
+
+								<div class="menu-day">
+									Wednesday
+								</div>
+								<div class="menu-price">
+									9
+								</div>
+							</div>
+							<div class="menu-name">
+								The Reuben Stuffed
+							</div>
+							<div class="menu-desc">
+								Creamy blends of goodness.
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header">
+								<div class="menu-day">
+									Thursday
+								</div>
+								<div class="menu-price">
+									9
+								</div>
+							</div>
+							<div class="menu-name">
+								Italian Beef Stuffed
+							</div>
+							<div class="menu-desc">
+								Creamy blends of goodness.
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header">
+								<div class="menu-day">
+									Friday
+								</div>
+								<div class="menu-price">
+									9
+								</div>
+							</div>
+							<div class="menu-name">
+								Buffalo Chicken Mac & Cheese Stuffed
+							</div>
+							<div class="menu-desc">
+								Creamy blends of goodness.
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-header">
+								<div class="menu-day">
+									Saturday
+								</div>
+								<div class="menu-price">
+									9
+								</div>
+							</div>
+							<div class="menu-name">
+								Pepperoni Pizza Stuffed
+							</div>
+							<div class="menu-desc">
+								Creamy blends of goodness.
+							</div>
 						</div>
 					</div>
 				</div>
@@ -202,115 +313,7 @@
 				</div>
 			</div>
 		</section>
-		<section class="daily-soups">
-			<div class="container">
-				<div class="section-header">
-					<h3>Daily Soups</h3>
-					<p>Our delicious mouth-watering soups that go hand in hand with your favorite pretzel.</p>
-				</div>
-				<div class="section-content">
-				<div class="cards">
-						<div class="card">
-							<div class="card-header">
-								<div class="menu-day">
-									Monday
-								</div>
-								<div class="menu-price">
-									8
-								</div>
-							</div>
-							<div class="menu-name">
-								French Onion
-							</div>
-							<div class="menu-desc">
-								Creamy blends of goodness.
-							</div>
-						</div>
-						<div class="card">
-							<div class="card-header">
-								<div class="menu-day">
-									Tuesday
-								</div>
-								<div class="menu-price">
-									9
-								</div>
-							</div>
-							<div class="menu-name">
-								Black Bean
-							</div>
-							<div class="menu-desc">
-								Creamy blends of goodness.
-							</div>
-						</div>
-						<div class="card">
-							<div class="card-header">
-
-								<div class="menu-day">
-									Wednesday
-								</div>
-								<div class="menu-price">
-									9
-								</div>
-							</div>
-							<div class="menu-name">
-								The Reuben Stuffed
-							</div>
-							<div class="menu-desc">
-								Creamy blends of goodness.
-							</div>
-						</div>
-						<div class="card">
-							<div class="card-header">
-								<div class="menu-day">
-									Thursday
-								</div>
-								<div class="menu-price">
-									9
-								</div>
-							</div>
-							<div class="menu-name">
-								Italian Beef Stuffed
-							</div>
-							<div class="menu-desc">
-								Creamy blends of goodness.
-							</div>
-						</div>
-						<div class="card">
-							<div class="card-header">
-								<div class="menu-day">
-									Friday
-								</div>
-								<div class="menu-price">
-									9
-								</div>
-							</div>
-							<div class="menu-name">
-								Buffalo Chicken Mac & Cheese Stuffed
-							</div>
-							<div class="menu-desc">
-								Creamy blends of goodness.
-							</div>
-						</div>
-						<div class="card">
-							<div class="card-header">
-								<div class="menu-day">
-									Saturday
-								</div>
-								<div class="menu-price">
-									9
-								</div>
-							</div>
-							<div class="menu-name">
-								Pepperoni Pizza Stuffed
-							</div>
-							<div class="menu-desc">
-								Creamy blends of goodness.
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
+		
 		<section class="sauces">
 			<div class="container">
 				<div class="section-header">
@@ -365,48 +368,68 @@
 		</section>
 		<section class="preorder-specials">
 			<div class="container">
-
 				<div class="section-header">
 					<h3>Preorder Specials</h3>
 					<p>Our special combos that require 24hr+ advanced pre-order and pre-payment. Not available in store.</p>
 				</div>
 				<div class="section-content">
-				<div class="cards">
+					<div class="cards">
 						<div class="card">
-							<div class="menu-image"><img src="/wp-content/themes/gnarlyknots/images/salted-pretzel.jpg" alt="salted"></div>
+							<div class="menu-image"><img src="/wp-content/themes/gnarlyknots/images/TBG-261.jpg" alt="salted"></div>
 							<div class="menu-name">Pretzel Bites</div>
 							<div class="menu-desc">Our delicious house favorite.</div>
 							<div class="menu-price">3</div>
 						</div>
 						<div class="card">
-							<div class="menu-image"><img src="/wp-content/themes/gnarlyknots/images/cheese-pretzel.jpg" alt="salted"></div>
+							<div class="menu-image"><img src="/wp-content/themes/gnarlyknots/images/TBG-198.jpg" alt="salted"></div>
 							<div class="menu-name">Mini Pretzels</div>
 							<div class="menu-desc">Our classic topped with aged cheese.</div>
 							<div class="menu-price">3.50</div>
 						</div>
 						<div class="card">
-							<div class="menu-image"><img src="/wp-content/themes/gnarlyknots/images/cheese-asiago.jpg" alt="salted"></div>
+							<div class="menu-image"><img src="/wp-content/themes/gnarlyknots/images/TBG-308.jpg" alt="salted"></div>
 							<div class="menu-name">Mini Giant Pretzels</div>
 							<div class="menu-desc">It's delicious.</div>
 							<div class="menu-price">4</div>
 						</div>
 						<div class="card">
-							<div class="menu-image"><img src="/wp-content/themes/gnarlyknots/images/cinnamon-pretzel.jpg" alt="salted"></div>
+							<div class="menu-image"><img src="/wp-content/themes/gnarlyknots/images/TBG-304.jpg" alt="salted"></div>
 							<div class="menu-name">Giant Pretzels</div>
 							<div class="menu-desc">It's delicious.</div>
 							<div class="menu-price">4</div>
 						</div>
 						<div class="card">
-							<div class="menu-image"><img src="/wp-content/themes/gnarlyknots/images/pretzeldog.jpg" alt="salted"></div>
+							<div class="menu-image"><img src="/wp-content/themes/gnarlyknots/images/cakepretz.jpeg" alt="salted"></div>
 							<div class="menu-name">Giant Birthday Cake Stuffed Pretzel</div>
 							<div class="menu-desc">It's delicious.</div>
 							<div class="menu-price">5.50</div>
 						</div>
 						<div class="card">
-							<div class="menu-image"><img src="/wp-content/themes/gnarlyknots/images/three-cheese.jpg" alt="salted"></div>
+							<div class="menu-image"><img src="/wp-content/themes/gnarlyknots/images/TBG-285.jpg" alt="salted"></div>
 							<div class="menu-name">Pretzel Buns</div>
 							<div class="menu-desc">It's delicious.</div>
 							<div class="menu-price">7</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="chips">
+			<div class="container">
+				<div class="section-header">
+					<h3>We Have Our Own Brand of<br>Gnarly Chips</h3>
+					<div class="section-description">Buy them in store today!</div>
+				</div>
+				<div class="section-content">
+					<div class="cards">
+						<div class="card">
+							<div class="shop-image"></div>
+						</div>
+						<div class="card">
+							<div class="shop-image"></div>
+						</div>
+						<div class="card">
+							<div class="shop-image"></div>
 						</div>
 					</div>
 				</div>
